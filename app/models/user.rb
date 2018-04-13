1a4bf16d629f88630917584192e7ed43967c833c
jonathan senior <jonosenior@gmail.com>
2018-04-13 13:11:41 +0200

    Add email format verification to user sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,10 @@ class User < ApplicationRecord
   has_many :posts
   before_create :create_token_for_user
   before_save {self.email = email.downcase}
-  validates :username, presence: true, length: {maximum: 50},
-  uniqueness: {case_sensitive:false}
-
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 30},
+  format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 4}
-
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email, presence: true, length: {maximum: 255},
-  # format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-
   has_secure_password
 
   def User.new_remember_token
